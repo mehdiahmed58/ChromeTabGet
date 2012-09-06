ee6aed761a22ad66beadb657ca44787869f01aa6
Pascal Paradis <pascal.paradis@gmail.com>
2012-09-06 18:56:11 -0400

    Mon premier plugin Chrome

---
+++ popup.js
@@ -0,0 +1,20 @@
+getLinks();
+
+function getLinks(){
+  var list = document.createElement("ul");
+
+  chrome.tabs.query({currentWindow:true}, function (tab){
+    for(x in tab)
+    {
+      console.log(tab[x].url);
+      var listElement = document.createElement("li");
+      var textlistElement= document.createTextNode(tab[x].url);
+
+      listElement.appendChild(textlistElement);
+      list.appendChild(listElement);
+    }  
+  });
+
+
+  document.body.appendChild(list);
+}
