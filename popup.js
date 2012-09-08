232121b9b0d01c082349ab1f8747969514482706
Pascal Paradis <pascal.paradis@gmail.com>
2012-09-08 13:02:22 -0400

    Mise à jour du code.

--- popup.js
+++ popup.js
@@ -4,8 +4,7 @@ function getLinks(){
   var list = document.createElement("ul");
 
   chrome.tabs.query({currentWindow:true}, function (tab){
-    for(x in tab)
-    {
+    for(x in tab){
       console.log(tab[x].url);
       var listElement = document.createElement("li");
       var textlistElement= document.createTextNode(tab[x].url);
@@ -14,7 +13,5 @@ function getLinks(){
       list.appendChild(listElement);
     }  
   });
-
-
   document.body.appendChild(list);
 }
